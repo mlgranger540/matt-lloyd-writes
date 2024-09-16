5dde3492a80d44d6ed62910f856645ddd3d4e9a3
mlgranger540 <mattgranger540@outlook.com>
2024-09-16 14:01:25 +0100

    Add function init files and Prismic connection

---
+++ functions/index.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-undef */
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const functions = require("firebase-functions");
+const express = require("express");
+const prismic = require("@prismicio/client");
+const fetch =  (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const repoName = 'matt-lloyd-writes' // Fill in your repository name.
+const accessToken = '' // If your repository is private, add an access token.
+
+// The `routes` property is your route resolver. It defines how you will 
+// structure URLs in your project. Update the types to match the Custom 
+// Types in your project, and edit the paths to match the routing in your 
+// project.
+const routes = [
+    {
+        type: 'post',
+        path: '/:uid',
+    },
+]
+
+const client = prismic.createClient(repoName, { 
+    fetch, 
+    accessToken,
+    routes,
+})
+
+const app = express();
+
+// Add a middleware function that runs on every route. It will inject 
+// the prismic context to the locals so that we can access these in 
+// our templates.
+app.use((req, res, next) => {
+    res.locals.ctx = {
+        prismic,
+    };
+    next();
+  });
+
+app.get("/getPosts", async (req, res)=>{
+    const documents = await client.getAllByType("post");
+    console.log(documents)
+    res.send(documents);
+});
+
+exports.mlw_app = functions.https.onRequest(app);
---
+++ public/assets/js/home.js
@@ -0,0 +1,81 @@
+window.onload = async function(){
+    const postsData = await fetch("/getPosts").then(function(response) {
+        // The response is a Response instance.
+        // You parse the data into a useable format using `.json()`
+        return response.json();
+    }).then(function(res) {
+        // Loop through post data from Prismic and add to post object
+        // then add object to posts array
+        let allPosts = [];
+        res.forEach((i) => {
+            let post = {};
+            let id = i.uid;
+            let title = i.data.title;
+            let rawDate = new Date(i.data.date_created);
+            let dayNum = rawDate.getDate();
+            let day = ordinalSuffix(dayNum);
+            let month = rawDate.toLocaleString('default', { month: 'long' });
+            let year = rawDate.getFullYear();
+            let dateWritten = day + " " + month + " " + year;
+            let content = i.data.content;
+            let tags = i.data.tags.split(',');
+            post.id = id;
+            post.title = title;
+            post.dateWritten = dateWritten;
+            post.content = content;
+            post.tags = tags;
+            allPosts.push(post);
+        });
+        
+        // Add posts to post section
+        let articleDiv = document.getElementById("article-div");
+        let quickNav = document.getElementById("quick-nav");
+        let article = '';
+        let quickNavLinks = '';
+        allPosts.forEach((post) => {
+            let id = post.id;
+            // Loop through title objects
+            let titleObjs = post.title;
+            let titles = [];
+            titleObjs.forEach((title) => {
+                title = title.text;
+                titles.push(title);
+            });
+            // Loop through content objects
+            let contentObjs = post.content;
+            let paragraphs = [];
+            contentObjs.forEach((paragraph) => {
+                paragraph = paragraph.text;
+                paragraphs.push(paragraph);
+            });
+            let dateWritten = post.dateWritten;
+            // Loop through tags and add hash
+            let tags = post.tags;
+            let hashtags = [];
+            tags.forEach((tag) => {
+                tag = '#' + tag;
+                hashtags.push(tag);
+            })
+
+            // Add data to article HTML
+            article += '<article id="' + id + '" class="inner-panel">';
+            article += '<h3>' + titles + '</h3>';
+            article += '<h4 class="entry-date">' + dateWritten + '</h4>';
+            paragraphs.forEach((paragraph) => {
+                article += '<p>' + paragraph + '</p>';
+            })
+            article += '<p class="tag">';
+            hashtags.forEach((tag) => {
+                article += tag + '&nbsp;&nbsp;';
+            })
+            article += '</p>';
+            article += '</article>';
+            article += '<div class="separator"><hr></div>';
+
+            // Create quick nav links
+            quickNavLinks += '<li><a href="#' + id + '">' + titles + '</a></li>';
+        });
+        articleDiv.innerHTML = article;
+        quickNav.innerHTML = quickNavLinks;
+    });
+}
